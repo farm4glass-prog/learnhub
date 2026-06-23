c6a3518ccd8dad4bcf724fe8625fe2824de8563a
farm4glass-prog <farm4glass@gmail.com>
2026-06-22 23:19:00 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,80 @@
-console.log("LearnFlow loaded");
+function openLesson(id){
+
+  const course =
+    courses.find(c => c.id === id);
+
+  lessonView.classList.remove("hidden");
+
+  let lessonsHTML = "";
+
+  course.lessons.forEach(lesson => {
+
+    if(lesson.type === "youtube"){
+
+      const videoId =
+        lesson.url.split("v=")[1];
+
+      const thumb =
+        `https://img.youtube.com/vi/${videoId}/hqdefault.jpg`;
+
+      lessonsHTML += `
+      <div class="lesson-card">
+
+        <img
+          src="${thumb}"
+          class="thumb"
+        >
+
+        <h3>${lesson.title}</h3>
+
+        <a
+          href="${lesson.url}"
+          target="_blank"
+        >
+          Watch Video →
+        </a>
+
+      </div>
+      `;
+    }
+
+    if(lesson.type === "pdf"){
+
+      lessonsHTML += `
+      <div class="lesson-card">
+
+        <h3>
+          📄 ${lesson.title}
+        </h3>
+
+        <a
+          href="${lesson.url}"
+          target="_blank"
+        >
+          Open PDF →
+        </a>
+
+      </div>
+      `;
+    }
+
+  });
+
+  lessonView.innerHTML = `
+  <div class="lesson">
+
+    <h2>
+      ${course.title}
+    </h2>
+
+    <p>
+      ${course.description}
+    </p>
+
+    <br>
+
+    ${lessonsHTML}
+
+  </div>
+  `;
+}
